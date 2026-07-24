d4e69fdc2b502913a5c16feb672aaaf68a57d82e
Muhammad Hariz Firhan <firiz030117@gmail.com>
2026-07-24 08:36:33 +0800

    Create clean_load_silver.erp_px_cat_g1v2.sql

---
+++ scripts/silver/clean_load_silver.erp_px_cat_g1v2.sql
@@ -0,0 +1,19 @@
+-->>LOAD
+INSERT INTO silver.erp_px_cat_g1v2(id,cat,subcat,maintenance)
+SELECT
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2;
+
+-->Check for catalog quality
+SELECT DISTINCT
+	cat
+FROM bronze.erp_px_cat_g1v2;
+
+-->>Check for unwanted space
+SELECT
+	maintenance
+FROM bronze.erp_px_cat_g1v2
+WHERE maintenance != TRIM(maintenance)
